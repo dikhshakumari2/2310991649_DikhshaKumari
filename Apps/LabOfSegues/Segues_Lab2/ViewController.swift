631d4f70af041e80cbc652e40de611fb1916184f
dikhshakumari2 <dikhsha1649.be23@chitkara.edu.in>
2025-07-19 15:20:51 +0530

    Added labs

---
+++ Apps/LabOfSegues/Segues_Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues_Lab2
+//
+//  Created by Student on 18/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
