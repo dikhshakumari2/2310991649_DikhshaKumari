cf891f5820bf80c91cbc3cb426b71350bc8399c5
dikhshakumari2 <dikhsha1649.be23@chitkara.edu.in>
2025-07-22 16:54:58 +0530

    Added apple pie project

--- Apps/LabOfSegues/Segues_Lab2/ViewController.swift
+++ Apps/LabOfSegues/Segues_Lab2/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondViewController" {
+            let destinationViewController = segue.destination as! SecondViewController
+            destinationViewController.messageToShow = "Hello from FirstViewController!"
+        }
     }
 
 
